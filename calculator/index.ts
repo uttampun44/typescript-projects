68dd65f5738be0570bba50b921492c5f89160848
uttam pun <uttampun50@gmail.com>
2023-11-16 20:48:47 +0545

    button value is set

--- calculator/index.ts
+++ calculator/index.ts
@@ -1,2 +1,16 @@
-let hello:string = "hello world"
-console.log(`hello`)
+
+let button = document.querySelectorAll('.button') as NodeListOf<HTMLElement>;
+let text = document.getElementById('calculator_text') as HTMLParagraphElement;
+
+
+
+button.forEach(calculatorButton => {
+    calculatorButton.addEventListener('click', function():void {
+
+      let buttonValue = this.getAttribute('value');
+
+      if(buttonValue === "0"){
+         text.innerText = "0";
+      }
+    })
+})
--- calculator/js/index.js
+++ calculator/js/index.js
@@ -1,3 +1,11 @@
 "use strict";
-let hello = "hello world";
-console.log(`hello`);
+let button = document.querySelectorAll('.button');
+let text = document.getElementById('calculator_text');
+button.forEach(calculatorButton => {
+    calculatorButton.addEventListener('click', function () {
+        let buttonValue = this.getAttribute('value');
+        if (buttonValue === "0") {
+            text.innerText = "0";
+        }
+    });
+});
