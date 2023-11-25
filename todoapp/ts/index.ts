de044a6c08c20cca3680a0a59891ad9a8d93461b
uttam pun <uttampun50@gmail.com>
2023-11-25 19:52:29 +0545

    to do app ui design finish

--- todoapp/ts/index.ts
+++ todoapp/ts/index.ts
@@ -1,1 +1,21 @@
-console.log("working")
+let addTask = document.querySelector('.add_task button') as HTMLButtonElement;
+let title = document.querySelector('#title') as HTMLInputElement;
+let details = document.querySelector('#details') as HTMLInputElement;
+
+// addtask function
+
+const addtask = ():void =>{
+
+    let tasks:string[] = [];
+
+    const taskTitle: string = title.value;
+    const taskDetail:string = details.value
+    tasks.push(taskTitle);
+    tasks.push(taskDetail)
+    console.log(tasks)
+    localStorage.setItem("do_to_app", JSON.stringify(tasks))
+}
+
+addTask.addEventListener('click', function add_task () {
+addtask()
+})
