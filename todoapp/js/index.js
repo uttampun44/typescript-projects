b9dec466a16ef057177822b0b884ea712d0b00ff
uttam pun <uttampun50@gmail.com>
2023-11-29 23:19:29 +0545

    append text finish

--- todoapp/js/index.js
+++ todoapp/js/index.js
@@ -2,15 +2,25 @@
 let addTask = document.querySelector('.add_task button');
 let title = document.querySelector('#title');
 let details = document.querySelector('#details');
+let title_detail = document.querySelector('.title_details');
+let title_para;
 // addtask function
 const addtask = () => {
     let tasks = [];
     const taskTitle = title.value;
     const taskDetail = details.value;
-    tasks.push(taskTitle);
-    tasks.push(taskDetail);
-    console.log(tasks);
-    localStorage.setItem("do_to_app", JSON.stringify(tasks));
+    if (taskTitle === "" || taskDetail === "") {
+        alert("Please enter the details");
+    }
+    else {
+        tasks.push(taskTitle);
+        tasks.push(taskDetail);
+        title_para = document.createElement("p");
+        title_detail.appendChild(title_para);
+        let task_join = tasks.join(" ");
+        title_detail.innerText = task_join;
+        localStorage.setItem("do_to_app", JSON.stringify(tasks));
+    }
 };
 addTask.addEventListener('click', function add_task() {
     addtask();
--- todoapp/ts/index.ts
+++ todoapp/ts/index.ts
@@ -1,7 +1,8 @@
 let addTask = document.querySelector('.add_task button') as HTMLButtonElement;
 let title = document.querySelector('#title') as HTMLInputElement;
 let details = document.querySelector('#details') as HTMLInputElement;
-
+let title_detail = document.querySelector('.title_details') as HTMLDivElement
+let title_para;
 // addtask function
 
 const addtask = ():void =>{
@@ -10,11 +11,24 @@ const addtask = ():void =>{
 
     const taskTitle: string = title.value;
     const taskDetail:string = details.value
-    tasks.push(taskTitle);
-    tasks.push(taskDetail)
-    console.log(tasks)
-    localStorage.setItem("do_to_app", JSON.stringify(tasks))
+
+    if(taskTitle === "" || taskDetail === ""){
+        alert("Please enter the details")
+    }else{
+        tasks.push(taskTitle);
+        tasks.push(taskDetail);
+
+        title_para = document.createElement("p") as HTMLDivElement;
+        title_detail.appendChild(title_para)
+
+           let task_join = tasks.join(" ");
+
+           title_detail.innerText = task_join
+
+            localStorage.setItem("do_to_app", JSON.stringify(tasks))
+     }
 }
+
 
 addTask.addEventListener('click', function add_task () {
 addtask()
