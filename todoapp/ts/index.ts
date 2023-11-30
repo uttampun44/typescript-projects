75fa257730f7f467d345bc1a1d2242a29c74c695
uttam pun <uttampun50@gmail.com>
2023-11-30 09:18:19 +0545

    save task in locastorage

--- todoapp/ts/index.ts
+++ todoapp/ts/index.ts
@@ -2,12 +2,13 @@ let addTask = document.querySelector('.add_task button') as HTMLButtonElement;
 let title = document.querySelector('#title') as HTMLInputElement;
 let details = document.querySelector('#details') as HTMLInputElement;
 let title_detail = document.querySelector('.title_details') as HTMLDivElement
-let title_para;
+
+let title_para
+let todo;
+let task:string [] = [];
 // addtask function
 
 const addtask = ():void =>{
-
-    let tasks:string[] = [];
 
     const taskTitle: string = title.value;
     const taskDetail:string = details.value
@@ -15,21 +16,35 @@ const addtask = ():void =>{
     if(taskTitle === "" || taskDetail === ""){
         alert("Please enter the details")
     }else{
-        tasks.push(taskTitle);
-        tasks.push(taskDetail);
+        task.push(taskTitle);
+        task.push(taskDetail);
 
-        title_para = document.createElement("p") as HTMLDivElement;
-        title_detail.appendChild(title_para)
 
-           let task_join = tasks.join(" ");
-
-           title_detail.innerText = task_join
-
-            localStorage.setItem("do_to_app", JSON.stringify(tasks))
+        localStorage.setItem("to_do_app", JSON.stringify(task))
      }
 }
 
 
+function displayTask(){
+     title_detail.innerText = ""
+
+     for(let taskItem of task){
+        title_para =  document.createElement("p") as HTMLDivElement;
+        title_detail.appendChild(title_para)
+        title_para.innerText = taskItem
+     }
+}
+
 addTask.addEventListener('click', function add_task () {
 addtask()
+displayTask()
 })
+
+window.addEventListener('load', () =>{
+     let local_items = localStorage.getItem("to_do_app");
+
+     if(local_items){
+        task = JSON.parse(local_items);
+        displayTask()
+     }
+});
