e3d79bd5c365650221d862a1b69f1dadc4ff1939
uttam pun <uttampun62@gmail.com>
2023-12-22 21:58:03 +0545

    convert temperature finish

--- temperature-converter/js/index.js
+++ temperature-converter/js/index.js
@@ -3,20 +3,22 @@ let value_text = document.querySelector('#value');
 let check_value = document.querySelector('#check-value');
 let select_option = document.querySelector("#select-option");
 let user_input = document.querySelector("#user_input");
-// function to calculate the value and display
-const displayValue = () => {
-    if (user_input) {
-        const inputVal = parseFloat(user_input.value);
-        const farenhite = (inputVal * 9 / 5) + 32;
-        value_text.innerText = ` Farenhite Value : ${farenhite.toString()}`;
+const selectOption = () => {
+    const selectElement = select_option.value;
+    if (selectElement === "celsius") {
+        console.log(selectElement);
+        const farenhiteVal = parseFloat(user_input.value);
+        const farenhite = (farenhiteVal - 32) * 5 / 9;
+        value_text.innerText = ` Farenhite Value : ${farenhite.toString()}°F`;
+        user_input.placeholder = "Please enter the farenhite";
+    }
+    else if (selectElement === "farenhite") {
+        console.log(selectElement);
+        const celsiusVal = parseFloat(user_input.value);
+        const celsius = (celsiusVal * 9 / 5) + 32;
+        value_text.innerText = `Farenhite Value : ${celsius.toLocaleString()}°C`;
+        user_input.placeholder = "Please enter the celsius";
     }
 };
-// select event
-select_option.addEventListener("change", () => {
-    user_input.value = "";
-    value_text.innerText = "";
-});
 // click to show the value
-check_value.addEventListener("click", function () {
-    displayValue();
-});
+check_value.addEventListener("click", selectOption);
--- temperature-converter/ts/index.ts
+++ temperature-converter/ts/index.ts
@@ -5,25 +5,26 @@ let select_option = document.querySelector("#select-option") as HTMLSelectElemen
 let user_input = document.querySelector("#user_input") as HTMLInputElement;
 
 
-// function to calculate the value and display
-const displayValue = () =>{
+const selectOption = ():void => {
+    const selectElement = select_option.value;
 
-    if(user_input){
-        const inputVal: number | bigint = parseFloat(user_input.value);
+    if (selectElement === "celsius") {
+        console.log(selectElement)
+        const  farenhiteVal: number | bigint = parseFloat(user_input.value);
 
-         const farenhite = (inputVal * 9/5) + 32;
+         const farenhite = (farenhiteVal -32 ) * 5/9;
+         value_text.innerText = ` Farenhite Value : ${farenhite.toString()}°F`;
+        user_input.placeholder = "Please enter the farenhite";
 
-         value_text.innerText = ` Farenhite Value : ${farenhite.toString()}`;
+    }else if(selectElement === "farenhite"){
+        console.log(selectElement)
+        const celsiusVal:number | bigint = parseFloat(user_input.value);
+        const celsius =  (celsiusVal * 9/5) + 32;
+        value_text.innerText = `Farenhite Value : ${celsius.toLocaleString()}°C`;
+        user_input.placeholder = "Please enter the celsius"
     }
+
 }
 
-// select event
-select_option.addEventListener("change", () =>{
-    user_input.value = ""
-    value_text.innerText = ""
-})
-
 // click to show the value
-check_value.addEventListener("click", function(){
- displayValue()
-})
+check_value.addEventListener("click", selectOption)
