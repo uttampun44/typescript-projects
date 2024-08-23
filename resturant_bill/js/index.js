ae437a1664d862d523e09a5503f3582f7c10e6a5
uttampun <uttampun50@gmail.com>
2024-08-23 08:28:46 +0545

    resturant_bill design done

--- resturant_bill/js/index.js
+++ resturant_bill/js/index.js
@@ -2,7 +2,7 @@
 let radioButton = document.querySelectorAll('.menuList input[type="radio"]');
 radioButton.forEach(btn => {
     btn.addEventListener("click", (val) => {
-        let inputValue = parseInt(btn.value);
+        let inputValue = parseFloat(btn.value,to);
         let tax = 0.13;
         console.log(`Button value ${inputValue}  `);
     });
