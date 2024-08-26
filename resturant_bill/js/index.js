fac1d109266ff8fabd907cb8948f6f660b88e1f4
uttampun <uttampun50@gmail.com>
2024-08-26 20:23:35 +0545

    calculating

--- resturant_bill/js/index.js
+++ resturant_bill/js/index.js
@@ -1,9 +1,22 @@
 "use strict";
 let radioButton = document.querySelectorAll('.menuList input[type="radio"]');
+let numberPerson = document.querySelector('#numberOfPerson');
+let calculateBtn = document.querySelector('#calculateButton');
+let totalbill = document.querySelector('.tax_included_btn .yourTotal');
 radioButton.forEach(btn => {
     btn.addEventListener("click", (val) => {
-        let inputValue = parseFloat(btn.value,to);
-        let tax = 0.13;
-        console.log(`Button value ${inputValue}  `);
+        let inputValue = Number(btn.value);
+        let vat = 0.13;
+        calculateBtn.addEventListener("click", () => {
+            let persons = parseInt(numberPerson.value, 10);
+            if (isNaN(persons) || persons <= 0)
+                return alert("Enter person");
+            let totalWithoutVAT = (inputValue + inputValue) * persons;
+            console.log(`Total amount without vat ${totalWithoutVAT}`);
+            let vatAmount = totalWithoutVAT * vat;
+            let totalWithVAT = totalWithoutVAT + vatAmount;
+            totalbill.innerText = totalWithVAT.toString();
+            console.log("Total with VAT:", totalWithVAT);
+        });
     });
 });
--- resturant_bill/ts/index.ts
+++ resturant_bill/ts/index.ts
@@ -1,12 +1,34 @@
 let radioButton = document.querySelectorAll('.menuList input[type="radio"]') as NodeListOf<HTMLInputElement>
+let numberPerson = document.querySelector('#numberOfPerson') as HTMLInputElement
+let calculateBtn = document.querySelector('#calculateButton') as  HTMLButtonElement
+let totalbill = document.querySelector('.tax_included_btn .yourTotal') as HTMLSpanElement;
 
 
 radioButton.forEach(btn => {
      btn.addEventListener("click", (val) =>{
-        let inputValue:number =  parseInt(btn.value);
-        let tax:number = 0.13
 
+      
+        let inputValue:number =  Number(btn.value);
+        let vat:number = 0.13
 
-        console.log(`Button value ${inputValue}  `)
+        calculateBtn.addEventListener("click", () =>{
+         let persons:number =  parseInt(numberPerson.value, 10)
+
+         if(isNaN(persons) || persons <= 0) return alert("Enter person")
+
+            let totalWithoutVAT = (inputValue + inputValue) * persons;
+    
+            console.log(`Total amount without vat ${totalWithoutVAT}`)
+         
+            let vatAmount = totalWithoutVAT * vat;
+            
+        
+            let totalWithVAT = totalWithoutVAT + vatAmount;
+
+            totalbill.innerText = totalWithVAT.toString()
+        
+            console.log("Total with VAT:", totalWithVAT);
+        })
      })
 })
+
